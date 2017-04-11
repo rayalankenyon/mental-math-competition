5e38201cb6187c85de1503e108ac35484616a51a
Rayalan Kenyon <rayalan.kenyon@gmail.com>
2017-04-11 14:11:23 -0500

    f

--- src/main/java/servlet/Game.java
+++ src/main/java/servlet/Game.java
@@ -76,7 +76,7 @@ public class Game extends HttpServlet
     	// question / answers form
     	out.print("<table><tr><td>");
     	int current_question = 1;
-    	rs = st.executeQuery("SELECT text FROM math.question WHERE id=" + current_question);
+    	rs = st.executeQuery("SELECT text FROM math.question WHERE id=1");
     	out.print(rs.getString("text"));
     	out.print("</td></tr></table>");
     	out.print("<br>");
