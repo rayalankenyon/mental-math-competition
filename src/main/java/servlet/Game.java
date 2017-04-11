0eb1012046df09fab09c747bd6696e235ba7be88
Rayalan Kenyon <rayalan.kenyon@gmail.com>
2017-04-11 08:14:48 -0500

    asdfewr

--- src/main/java/servlet/Game.java
+++ src/main/java/servlet/Game.java
@@ -33,7 +33,7 @@ public class Game extends HttpServlet
 		out.print("username: <input type='text' name='username' required><br>");
 		out.print("password: <input type='password' name='password' required><br>");
 		out.print("<input type='submit' value='Login'> or <input type='button' onclock='' value='Create Account'>");
-		out.print(</form>");
+		out.print("</form>");
     	out.print("</td></tr></table>");
     } else {
     	// logout
