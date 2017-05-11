2ad7f355e16843eba4464f4ed50f9a87d35b5307
Rayalan Kenyon <rayalan.kenyon@gmail.com>
2017-05-11 00:44:11 -0500

    div

--- src/main/java/servlet/Game.java
+++ src/main/java/servlet/Game.java
@@ -90,7 +90,7 @@ public class Game extends HttpServlet
             out.print("</ul>");
 
             out.print("<div class='container'>");
-            out.print("<form class='navbar-form navbar-right' action='/' method='post'><input class='form-control btn btn-primary' type='submit' value='Refresh'></form>");
+            out.print("<form class='navbar-form navbar-right' action='/' method='post'><div class='form-group'><input class='form-control btn btn-primary' type='submit' value='Refresh'></div></form>");
             out.print("</div>");
             out.print("</nav>");
 
